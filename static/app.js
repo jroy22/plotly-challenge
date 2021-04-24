bce3969aac6f06ad779f51d8fc369d279660c136
Jeannaej <jeannaejy22@gmail.com>
2021-04-24 18:14:50 -0500

    Added function stubs to organize and prepare for functions

--- static/app.js
+++ static/app.js
@@ -6,25 +6,35 @@ console.log("app.js loaded");
 function InitDashboard(){
     console.log("Init Dashboard()");
 
-    //Populate the dropdown
+    // Populate the dropdown
         var selector = d3.select("#selDataset");
 
         d3.json("./data/samples.json").then(function (data) {
             console.log(data);
             
             var sampleName = data.names;
+            
+            // For each to get the Sample Ids on the dropdown
             sampleName.forEach(sampleId => {
                 selector.append("option")
                         .text(sampleId)
                         .property("value", sampleId);
             });
+        
+            var id = sampleNames[0];
+
+            // Function stubs
+            DrawBargraph(id);
+            DrawBubbleChart(id);
+            CreateDemoTable(id);
+            //DrawGauge(id);
 
         });
 
 
-    //Update the bargraph
-    //Update Bubble Graph
-    //Update Demographic Info
+    // Update the bargraph
+    // Update Bubble Graph
+    // Update Demographic Info
 }
 
 InitDashboard();
