4eef8e1c5a8481c5b02ed3a4ef49cd165aae0c02
Jeannaej <jeannaejy22@gmail.com>
2021-04-24 18:04:11 -0500

    Code the has an outline of what needs to be created

---
+++ static/app.js
@@ -0,0 +1,15 @@
+// Check to see whether app.js is being read
+console.log("app.js loaded");
+
+// Code credit to Dom during Office Hours on 4/17/2021
+
+function InitDashboard(){
+    console.log("Init Dashboard()");
+
+    //Populate the dropdown
+    //Update the bargraph
+    //Update Bubble Graph
+    //Update Demographic Info
+}
+
+InitDashboard();
